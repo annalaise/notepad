4c8e004eff1c0c9dc3caf6423386b99e6cfd660a
theartofnowt <michaelgordon.brown1@gmail.com>
2017-08-15 12:31:22 +0100

    test passed for note-list-model

---
+++ tests/note-list-model-spec.js
@@ -0,0 +1,27 @@
+(function(exports) {
+  console.log("test to initiatise a note with list model")
+  function testNoteModelList() {
+    var list = new List();
+
+    if (list.notes() === null) {
+      throw new Error("no list")
+    }
+  };
+
+   testNoteModelList();
+   console.log("test passes")
+})(this);
+
+(function(exports) {
+  console.log("Test to addNote to list");
+  function testAddNoteToListModel() {
+    var list = new List();
+    list.addNote('text');
+
+    if(list.notes()[0].text !== 'text') {
+      throw new Error("no note added");
+    }
+  }
+  testAddNoteToListModel();
+  console.log("test passes");
+})(this);
