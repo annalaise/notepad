94e3fd768a7b57c5004d4130facffa9d1b38554e
Ryan Davidson <daviryan@aol.com>
2017-08-16 11:17:29 +0100

    Spike add controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,1 +1,16 @@
 document.getElementById('app').innerHTML = 'howdy';
+
+(function(exports){
+  function NoteController(list) {
+    this.noteListView = new View(list);
+  }
+  NoteController.prototype.display = function() {
+    exports.document.getElementById('app').innerHTML = this.noteListView.listHtml();
+  };
+  exports.NoteController = NoteController;
+})(this);
+
+var note = new Note("I love Javascript");
+var list = new List().addNote(note);
+new NoteController(list).display();
+console.log("Success!!");
