4ccaf94ecf17e717885da11d2875bdef5839613e
annalaise <annalaise.gibbons@gmail.com>
2017-08-16 08:39:45 +0100

    note controller progress

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-
   function noteController() {
-    $(document).getElementById("app").innerHTML = ("howdy");
-  };
+    document.getElementById('app').innerHTML = 'howdy';
+  }
 
-}
+window.onload = noteController
